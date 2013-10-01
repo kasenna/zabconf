3ea6a086f3de127c72db733101906fd00fcaab09
kasenna <mkarpenkov@2reallife.com>
2013-10-01 19:02:29 +0400

    Two methods in API added plus host create implemented

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 
 	"fmt"
-	"github.com/zabbix"
+	"zabconf/zabbix_api"
 	
 )
 
@@ -30,17 +30,38 @@ func main() {
     fmt.Println(versionresult)
 
     params := make(map[string]interface{}, 0)
-    f1 := make(map[string]string, 0)
-    o1 := make(map[string]string, 0)
-    f1["host"] = "Kasenna MacBook"
-    o1["h1"] = "status"
-    o1["h2"] = "available"
-    o1["h3"] = "host"
+
+//    f1 := make(map[string]string, 0)
+//    o1 := make(map[string]string, 0)
+
+    groups := make(map[string]string, 0)
+    templates := make(map[string]string, 0)
+    interfaces := make(map[string]string, 0)
+
+//    f1["host"] = "Template OS Mac OS X"
+//    o1["h1"] = "status"
+//    o1["h2"] = "available"
+//    o1["h3"] = "host"
+
+    groups["groupid"] = "6"
+    templates["templateid"] = "10079"
+    interfaces["type"] = "1"
+    interfaces["main"] = "1"
+    interfaces["useip"] = "1"
+    interfaces["ip"] = "192.168.160.166"
+    interfaces["dns"] = ""
+    interfaces["port"] = "10050"
+
 //    params["filter"] = f1
-    params["output"] = o1
+    params["host"] = "Kasenna MacBook"
+    params["interfaces"] = interfaces
+    params["groups"] = groups
+    params["templates"] = templates
+
+//    params["output"] = "extend"
 //    params["templated_hosts"] = 1
     
-    ret, err := api.Host("get", params)
+    ret, err := api.Host("create", params)
     
     if err != nil {
 	fmt.Println(err)
